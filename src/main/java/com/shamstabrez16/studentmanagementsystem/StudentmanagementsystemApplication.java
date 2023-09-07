895b6cb75a17afaf688d69fb4289f4cfe5d7a542
Mohammad Shams Tabrez <shamstabrez.raw@gmail.com>
2023-09-07 16:11:55 -0500

    CI/CD Testing Jenkins - 1

--- src/main/java/com/shamstabrez16/studentmanagementsystem/StudentmanagementsystemApplication.java
+++ src/main/java/com/shamstabrez16/studentmanagementsystem/StudentmanagementsystemApplication.java
@@ -1,17 +1,10 @@
 package com.shamstabrez16.studentmanagementsystem;
 
-import com.shamstabrez16.studentmanagementsystem.service.StudentService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
-import org.springframework.ws.config.annotation.EnableWs;
-import org.springframework.ws.transport.http.MessageDispatcherServlet;
+
 
 @SpringBootApplication
-@EnableWs
 public class StudentmanagementsystemApplication {
 
 	public static void main(String[] args) {
